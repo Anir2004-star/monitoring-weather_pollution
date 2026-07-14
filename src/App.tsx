e5d4f5b105ec3fb9e3c8b0b4cdbfd258ec27b332
Anir2004-star <anirban.choudhury.2004@gmail.com>
2026-07-14 20:42:43 +0530

    fix: Disable solar theme to enforce cinematic dark aesthetic globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
-import useSolarTheme from './hooks/useSolarTheme';
 
 const App = () => {
-  useSolarTheme();
-
   return <RouterProvider router={router} />;
 };
 
